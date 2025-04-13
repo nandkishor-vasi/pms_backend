f722d7cdea4e3f13d3f3e63e57d64cf00f0df535
nandkishor-vasi <nandkishorvasi@gmail.com>
2025-04-13 12:33:14 +0530

    backend logic for signup & login

--- backend/src/main/java/com/example/CepDemo1/config/JwtFilter.java
+++ backend/src/main/java/com/example/CepDemo1/config/JwtFilter.java
@@ -25,7 +25,6 @@ public class JwtFilter extends OncePerRequestFilter {
     @Autowired
     ApplicationContext context;
 
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String authHeader = request.getHeader("Authorization");
